bca4675a7c91426dd9ed08e10ca9a20f9da9dece
Cooper <104477358+Cwooper@users.noreply.github.com>
2024-05-03 18:13:36 -0700

    Initialized repo

---
+++ README.md
@@ -0,0 +1,34 @@
+# Current Install Instructions
+Cooper Morgan
+05/03/2024
+
+## Current directory tree
+
+```
+home/cwooper
+├── 305
+├── 347
+└── vault
+    ├── schedule-optimizer
+    ├── srg-synvis
+    └── install-instructions
+        ├── install_packages.sh
+        ├── packages.txt
+        └── tree.md
+```
+
+## Current Directories
+git clone
+https://github.com/Cwooper/install-instructions
+
+https://github.com/Cwooper/305
+
+https://gitlab.cs.wwu.edu/gibbonj9/csci347_s24
+
+mkdir vault
+cd vault
+
+git clone
+https://github.com/Cwooper/schedule-optimizer
+
+https://github.com/cs-wwu/srg-synvis
---
+++ install_packages.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# Cooper Morgan
+# Run this program with sudo
+# This will `sudo apt install` all packages in packages.txt
+# 
+# This program assumes that you have already added:
+# deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+# to your /etc/apt/sources.list
+#
+# Also make sure to run sudo gpasswd -a $USER plugdev
+# for openrazer driver to take effect
+#
+# Finish installing vpn here: 
+# https://support.cs.wwu.edu/home/access/wwu_vpn/linux_bsd/wwu_nm.html
+#
+# Reboot the system after installing packages
+#
+
+
+# Color codes
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Check if the script is run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+# Check if packages.txt exists
+if [ ! -f "packages.txt" ]; then
+    echo "packages.txt not found!"
+    exit 1
+fi
+
+# Check if sources.list exists
+sources_list="/etc/apt/sources.list"
+if [ ! -f "$sources_list" ]; then
+    echo "sources.list not found!"
+    exit 1
+fi
+
+# Update the OS first
+apt update -y
+
+# Read packages from packages.txt and reinstall them
+while IFS= read -r line
+do
+    # Skip empty lines and comment lines
+    if [[ -z "$line" || "$line" == \#* ]]; then
+        continue
+    fi
+    
+    package=$(echo "$line" | awk '{print $1}') # Extract the package name (in case there are extra parameters)
+    
+    # Install package silently and check exit status
+    apt -qq install -y "$package"
+    if [ $? -eq 0 ]; then
+        echo -e "${GREEN}$package installed successfully.${NC}"
+    else
+        echo -e "${RED}$package installation failed.${NC}"
+    fi
+done < "packages.txt"
+
+apt -qq autoremove -y
+
+echo "All packages installed or attempted to install."
